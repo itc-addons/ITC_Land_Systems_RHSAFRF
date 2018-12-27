81bcb426e3bfee88e8dbcc9d1f707bfbf6fe00d3
Andrew Somerville <asmsandrew@googlemail.com>
2018-12-27 23:12:38 +0000

    D30 / 2A18M - BTY Define missed

---
+++ Addons/itc_land_rhsafrf_d30/CfgITCLandBalistics.hpp
@@ -0,0 +1,26 @@
+class itc_land_ballistics {
+  class batteryTypes {
+    class RHS_2A18M {
+		displayName = "2A18M - 122mm Howitzer";
+		type = "cannon";
+		ammunition[] = {
+			"rhs_mag_of462", 
+			"rhs_mag_of462_10",
+			"rhs_mag_3of56", 
+			"rhs_mag_3of56_10", 
+			"rhs_mag_bk6m", 
+			"rhs_mag_bk6m_26", 
+			"rhs_mag_bk13", 
+			"rhs_mag_bk13_26", 
+			"rhs_mag_d462", 
+			"rhs_mag_d462_2", 
+			"rhs_mag_s463", 
+			"rhs_mag_s463_2", 
+			"rhs_mag_3of69m", 
+			"rhs_mag_3of69m_2"
+		};
+		weaponClass = "rhs_weap_D30";
+    };
+  };
+};
+
