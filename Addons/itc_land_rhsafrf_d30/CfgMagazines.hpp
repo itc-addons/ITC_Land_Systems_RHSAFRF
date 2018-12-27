cbfa05ddc83d43ee112e09dac90b19b36a86ae03
Andrew Somerville <asmsandrew@googlemail.com>
2018-12-27 23:12:16 +0000

    D30 / 2A18M - Added BCS Compatibility
    BCS can now be used to calculate solutions for RHS AFRF D30 / 2A18M 122mm Howitzer

---
+++ Addons/itc_land_rhsafrf_d30/CfgMagazines.hpp
@@ -0,0 +1,18 @@
+class CfgMagazines {
+	class 32Rnd_155mm_Mo_shells;
+	class rhs_mag_of462: 32Rnd_155mm_Mo_shells {
+		itc_land_btabListFile = "itc_land_rhsafrf_d30\tables\list.sqf";
+	};
+	class rhs_mag_of462_10: rhs_mag_of462 {
+		itc_land_btabListFile = "itc_land_rhsafrf_d30\tables\list.sqf";
+	};
+	class rhs_mag_3of56: rhs_mag_of462 {
+		itc_land_btabListFile = "itc_land_rhsafrf_d30\tables\list.sqf";
+	};
+	class rhs_mag_d462: rhs_mag_of462 {
+		itc_land_btabListFile = "itc_land_rhsafrf_d30\tables\list.sqf";	
+	};
+	class rhs_mag_s463: rhs_mag_of462 {
+		itc_land_btabListFile = "itc_land_rhsafrf_d30\tables\list.sqf";	
+	};	
+};
---
+++ Addons/itc_land_rhsafrf_d30/config.cpp
@@ -0,0 +1,14 @@
+class CfgPatches {
+	class itc_land_rhsafrf_d30 {
+		author = "RHS + ITC Addons Team";
+		authors[] = {"RHS","ToadBall","Yax"};
+		requiredaddons[] = {"rhs_main", "A3_CargoPoses_F", "A3_Weapons_F", "A3_Weapons_F_gamma", "A3_Weapons_F_epc","rhs_c_heavyweapons"};
+		requiredversion = 1.8;
+		units[] = {};
+		weapons[] = {};
+		magazines[] = {};
+	};
+};
+
+#include "CfgMagazines.hpp"
+#include "CfgITCLandBalistics.hpp"
