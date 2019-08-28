ea1013db3bd9636165b85c700d713155f79d630c
Andrew Somerville <asmsandrew@googlemail.com>
2019-08-28 23:38:28 +0100

    Config tweaked to retain 2S3 compatibility with ammohandler

--- Addons/itc_land_rhsafrf_2s3/CfgVehicles.hpp
+++ Addons/itc_land_rhsafrf_2s3/CfgVehicles.hpp
@@ -16,30 +16,30 @@
 class CfgVehicles {
 	class LandVehicle;
 	class Tank: LandVehicle {
-        class ACE_SelfActions;	
+        class ACE_SelfActions;
 	};
 	class Tank_F: Tank {
-        class ACE_SelfActions: ACE_SelfActions {};		
+        class ACE_SelfActions: ACE_SelfActions {};
 		class Turrets {
 			class MainTurret;
 		};
 		class AnimationSources;
 	};
 	class rhs_2s3tank_base: Tank_F {
-        class ACE_SelfActions: ACE_SelfActions {};		
+        class ACE_SelfActions: ACE_SelfActions {};
 		class Turrets: Turrets {
 				class MainTurret: MainTurret {};
 		};
 		class AnimationSources: AnimationSources {};
 	};
 	class rhs_2s3_tv: rhs_2s3tank_base {
-        class ACE_SelfActions: ACE_SelfActions {};		
+        class ACE_SelfActions: ACE_SelfActions {};
 		class Turrets: Turrets {
 			class MainTurret: MainTurret {};
 		};
-		class AnimationSources: AnimationSources {};		
+		class AnimationSources: AnimationSources {};
 	};
-	
+
 	class itc_land_rhsafrf_2s3_tv: rhs_2s3_tv {
 		scopeCurator = 2;
 		displayname = "2S3 mod. 0 Akatsiya 2";
@@ -48,16 +48,17 @@ class CfgVehicles {
                 displayName = "Open Ammo Handling Interface";
                 condition = "( gunner _target ) == ACE_Player";
                 statement = "createDialog 'ITC_Land_SPHammohandler'";
-            };	
+            };
 			class ITC_Land_CommanderTablet {
 				displayName = "Open Mounted Tablet";
 				icon = "\itc_land_tablet\UI\arty-icon.paa";
 				condition = "([_target] call itc_land_tablet_fnc_vehicleHasTablet) && (( commander _target ) == ACE_Player)";
 				statement = "[_target] call itc_land_tablet_fnc_openVehicleTablet";
-			};			
-		};				
+			};
+		};
 		artilleryScanner = 0;
 		class itc_land {
+  		  loaderType = 1; // semi automatic			
 			tabletInterfaces[] = {"spg"};
 			mountedTablet = "itc_land_tablet_spg";
 		};
@@ -70,14 +71,14 @@ class CfgVehicles {
 					mag_4("itc_land_2a33hex"),
 					mag_5("itc_land_2a33smo"),
 					mag_5("itc_land_2a33ill"),
-					mag_3("itc_land_2a33icm"),									
+					mag_3("itc_land_2a33icm"),
 					mag_3("itc_land_2a33lgm")
 				};
 				lockWhenVehicleSpeed = 1;
 				maxHorizontalRotSpeed = "((360/30)/45)";
 			};
 		};
-		class AnimationSources: AnimationSources {};				
+		class AnimationSources: AnimationSources {};
 	};
 
 };
